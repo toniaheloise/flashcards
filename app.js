5514b0867937d63331b12f6c09e0d71ea9802b6e
toniaheloise <nascimento.antonia@escola.pr.gov.br>
2025-07-30 12:53:01 +0000

    criamos as perguntas no jacsgrifts

---
+++ app.js
@@ -0,0 +1,5 @@
+function criacartao(categoria,pergunta,resposta){
+const container = document.getElementById("container")
+const cartao = document.createElement("article")
+ cartao.classname = 'cartao'
+}
---
+++ assets/perguntas.js
@@ -0,0 +1,34 @@
+criaCartao(
+"Historia",
+"Qual foi o evento que marcou o início da Primeira Guerra Mundial?",
+"O assassinato do arquiduque Francisco Ferdinando da Áustria-Hungria em Sarajevo"
+)
+
+criaCartao(
+"Historia",
+"Quem foi a primeira pessoa a viajar para o espaço?",
+" Yuri Gagarin"
+)
+
+criacartao(
+"Historia",
+" Em que ano a Revolução Francesa começou?",
+" 1789."
+) 
+criacartao( 
+"Historia",
+"Qual império dominou a maior parte da América do Sul na época da conquista europeia?",
+"O Império Inca"
+)
+criacartao(
+"Historia",
+"Quem foi o líder do movimento pelos direitos civis nos Estados Unidos?",
+" Martin Luther King Jr."
+)
+
+criacartao(
+"Historia"
+""
+
+""
+)
