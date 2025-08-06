038fc0e7d256718c8ff9905de3a1306768849773
toniaheloise <nascimento.antonia@escola.pr.gov.br>
2025-08-06 13:56:31 +0000

    fizemos cartoes javascript

--- app.js
+++ app.js
@@ -2,4 +2,20 @@ function criacartao(categoria,pergunta,resposta){
 const container = document.getElementById("container")
 const cartao = document.createElement("article")
  cartao.classname = 'cartao'
+
+ cartao.innerHTML =`<div class="cartao_conteudo"> 
+                <h3>${ categoria} </h3>
+                <div class="pergunta"> 
+                    ${pergunta}
+                </div>
+                <div class="resposta"> 
+                    ${resposta}
+
+                </div>
+            </div> `
+
+
+        container.appendChild(cartao)
+
+
 }
